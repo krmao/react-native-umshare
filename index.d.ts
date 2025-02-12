e6d31864fe46e7522d25d5068f16fa2a16d58c19
kr.mao <kr.mao@qq.com>
2025-02-12 11:34:48 +0800

    feat: android new sdks 0.0.4

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,50 @@
 // noinspection SpellCheckingInspection
 
-declare module '@krmao/react-native-umshare' {
+declare module "@krmao/react-native-umshare" {
     export const ShareUtil: {
-        share: (text: string, img: any, weburl: string, title: string, sharemedia: any, successCallback: any) => void
-        auth: (sharemedia: any, successCallback: any) => void
-        shareboard: (text: string, img: any, weburl: string, title: string, sharemedias: any, successCallback: any) => void
+        init: (platformConfig: { [key in keyof typeof SHARE_MEDIA]: any }) => void,
+        share: (text: string, img: string | undefined | null, weburl: string, title: string, sharemedia: string, successCallback: Callback) => void,
+        auth: (sharemedia: any, successCallback: Callback) => void,
+        shareboard: (text: string, img: string | undefined | null, weburl: string, title: string, sharemedias: string[], successCallback: Callback) => void,
     };
-    export const Callback: any;
+    export type Callback = (...args: any[]) => void;
+    export const SHARE_MEDIA: {
+        GENERIC: "GENERIC",
+        SMS: "SMS",
+        EMAIL: "EMAIL",
+        SINA: "SINA",
+        QZONE: "QZONE",
+        QQ: "QQ",
+        WEIXIN: "WEIXIN",
+        WEIXIN_CIRCLE: "WEIXIN_CIRCLE",
+        WEIXIN_FAVORITE: "WEIXIN_FAVORITE",
+        WXWORK: "WXWORK",
+        DOUBAN: "DOUBAN",
+        FACEBOOK: "FACEBOOK",
+        FACEBOOK_MESSAGER: "FACEBOOK_MESSAGER",
+        TWITTER: "TWITTER",
+        YIXIN: "YIXIN",
+        YIXIN_CIRCLE: "YIXIN_CIRCLE",
+        INSTAGRAM: "INSTAGRAM",
+        PINTEREST: "PINTEREST",
+        EVERNOTE: "EVERNOTE",
+        POCKET: "POCKET",
+        LINKEDIN: "LINKEDIN",
+        FOURSQUARE: "FOURSQUARE",
+        YNOTE: "YNOTE",
+        WHATSAPP: "WHATSAPP",
+        LINE: "LINE",
+        FLICKR: "FLICKR",
+        TUMBLR: "TUMBLR",
+        ALIPAY: "ALIPAY",
+        KAKAO: "KAKAO",
+        BYTEDANCE: "BYTEDANCE",
+        BYTEDANCE_PUBLISH: "BYTEDANCE_PUBLISH",
+        BYTEDANCE_FRIENDS: "BYTEDANCE_FRIENDS",
+        DROPBOX: "DROPBOX",
+        VKONTAKTE: "VKONTAKTE",
+        DINGTALK: "DINGTALK",
+        HONOR: "HONOR",
+        MORE: "MORE"
+    };
 }
